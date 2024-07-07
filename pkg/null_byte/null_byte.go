e5495d3b9e982e07a3224981b58a054d8133eb05
shellyyg <chih-ching.yang@whu.edu>
2024-07-07 17:42:50 +1000

    Add check null byte functionality

---
+++ pkg/null_byte/null_byte.go
@@ -0,0 +1,16 @@
+package null_byte
+
+import "strings"
+
+type Validator struct{}
+
+func NewValidator() *Validator{
+	return &Validator{}
+}
+
+func (iv *Validator) ContainsNullByte(input string) bool {
+	if input == "" {
+		return false
+	}
+	return strings.Contains(input,"\x00")	
+}
