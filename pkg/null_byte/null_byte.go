fa1af91c84902be06769ee131cf5892d196255ce
shellyyg <chih-ching.yang@whu.edu>
2024-07-07 17:44:41 +1000

    Null byte example request

--- pkg/null_byte/null_byte.go
+++ pkg/null_byte/null_byte.go
@@ -7,7 +7,8 @@ type Validator struct{}
 func NewValidator() *Validator{
 	return &Validator{}
 }
-
+// Note that http://localhost:8084?input=%00 is a null byte 
+// It does not look like a null byte, because in http request, we need to encode it in UTF-8 format in order to send it
 func (iv *Validator) ContainsNullByte(input string) bool {
 	if input == "" {
 		return false
