925adb37e46ef13e6301f966d9844b509851f46d
shellyyg <chih-ching.yang@whu.edu>
2024-07-07 16:56:05 +1000

    Add boundary checking functionality

---
+++ pkg/calculator/calculator.go
@@ -0,0 +1,11 @@
+package calculator
+
+type Calculator struct{}
+
+func NewCalculator() *Calculator{
+	return &Calculator{}
+}
+
+func (c *Calculator) Add(num1, num2 int) (int) {
+	return num1 + num2
+}
---
+++ pkg/validator/validator.go
@@ -0,0 +1,25 @@
+package validator
+
+import (
+	"fmt"
+	"strconv"
+)
+
+type IntegerValidator struct{}
+
+func NewIntegerValidator() *IntegerValidator{
+	return &IntegerValidator{}
+}
+
+func (s *IntegerValidator) Validate(value string) (int, error) {
+	num, err := strconv.Atoi(value)
+
+	if err != nil {
+		return 0, fmt.Errorf("invalid input")
+	}
+	if num < 1 || num > 100 {
+		return 0, fmt.Errorf("input out of range")
+	}
+
+	return num, nil
+}
