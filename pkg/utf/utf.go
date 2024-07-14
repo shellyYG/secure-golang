2bdde2b114f82fb160ba4fb17a4ecfb31b9429f7
shellyyg <chih-ching.yang@whu.edu>
2024-07-14 18:13:02 +1000

    Add check-is-valid UTF-8 encode string function

---
+++ pkg/utf/utf.go
@@ -0,0 +1,13 @@
+package utf
+
+import "unicode/utf8"
+
+type Utf struct{}
+
+func NewUtf() *Utf {
+	return &Utf{}
+}
+
+func (*Utf) InputIsValidUtf8(input string) bool {
+	return utf8.ValidString(input)
+}
