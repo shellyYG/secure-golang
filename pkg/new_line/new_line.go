f2471d9883c54e9bf3bdcceb6bf88e54a2194145
shellyyg <chih-ching.yang@whu.edu>
2024-07-14 17:57:24 +1000

    Add check new line char functionality

---
+++ pkg/new_line/new_line.go
@@ -0,0 +1,14 @@
+package new_line
+
+import "strings"
+
+type Validator struct{}
+
+func NewValidator() *Validator {
+	return &Validator{}
+}
+
+func (*Validator) ContainsNewLine(input string) bool {
+	index := strings.IndexByte(input, '\n')
+	return index >= 0
+}
