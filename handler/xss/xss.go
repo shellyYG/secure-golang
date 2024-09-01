2f3a894ece0d583043b6cc97ceba6621ec067e05
shellyyg <chih-ching.yang@whu.edu>
2024-09-01 15:39:20 +1000

    Fix XSS attack using html/template to encode the output

--- handler/xss/xss.go
+++ handler/xss/xss.go
@@ -2,7 +2,7 @@ package main
 
 import (
 	"fmt"
-	"io"
+	"html/template"
 	"net/http"
 )
 
@@ -11,8 +11,21 @@ type Handler struct{
 
 func (h *Handler) AddHandler(w http.ResponseWriter, r *http.Request) {
 	input := r.URL.Query().Get("input")
-	output := "<h1>Hello, " + input + "</h1>"
-	io.WriteString(w, output)
+	// Below will create a template that will be used to render the output
+	// This is safe because the template is not executed with user input
+	tmp, err := template.New("output").Parse("<h1>Hello :D, {{ . }}</h1>")
+	if err != nil {
+		fmt.Printf("Error parsing template: %s\n", err)
+		return
+	}
+	// Below in tmp.Execute, we are executing the template with user input, but as we can see, 
+	// the template is not using the user input directly, it is just using the user input as a variable
+	// so the user input is not executed as code
+	err = tmp.Execute(w, input)
+	if err != nil {
+		fmt.Printf("Error executing template: %s\n", err)
+		return
+	}
 }
 
 func AvoidXSSAttack() {
